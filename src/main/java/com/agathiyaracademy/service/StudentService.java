af55a0ea4779025a3185df282e13a23684622827
manoj <kannamanoj2@gmail.com>
2023-10-17 23:11:46 +0530

    Update StudentService.java

--- src/main/java/com/agathiyaracademy/service/StudentService.java
+++ src/main/java/com/agathiyaracademy/service/StudentService.java
@@ -25,12 +25,14 @@ public class StudentService {
         if (isValidStudentData(studentRequest) && userService.isEmailIdRegistered(studentRequest.emailId())) {
             User user = null;
             user = userService.fetchUserByEmailId(studentRequest.emailId());
-            if (user != null) {
+            if (user != null && user.getStudent() == null) {
                 Student student = null;
                 student = Student.builder().rollNumber(studentRequest.rollNumber()).isFeePaid(studentRequest.isFeePaid()).address(studentRequest.address()).tnpscGroup(studentRequest.tnpscGroup()).tnpscRegistrationNumber(studentRequest.tnpscRegistrationNumber()).build();
+                // save the student to the database
                 studentRepository.save(student);
+                // after we save the student now we're set the user to the student
                 student.setUser(user);
-//                TODO( we created the user in given entity and also perfectly created student entity but the student will create duplicate)
+                // save the student to update in database
                 studentRepository.save(student);
                 studentResponse = ConstantRecord.StudentResponse.builder().name(student.getUser().getName()).emailId(student.getUser().getEmailId()).address(student.getAddress()).phoneNumber(student.getUser().getPhoneNumber()).tnpscGroup(student.getTnpscGroup()).isFeePaid(student.isFeePaid()).rollNumber(student.getRollNumber()).tnpscRegistrationNumber(student.getTnpscRegistrationNumber()).build();
             }
